1004fb50cc2a00d27e9189a941c712e16a16f128
Kamau-ke <kamauwamkuu@gmail.com>
2024-10-14 08:53:31 +0300

    working on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 // import Register from'./pages/register'
 import Navbar from './components/Navbar'
+
 import './App.css'
 function App() {
  
 
   return (
-    <>
+    <div className='h-screen w-screen bg-white'>
     {/* <Register/> */}
     <Navbar/>
-    </>
+
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,46 +4,69 @@ import Profile from '../assets/images/profile.jpg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBell } from '@fortawesome/free-regular-svg-icons'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
 
-// import '../App.css'
+// // import '../App.css'
 function Navbar() {
   return (
-    <div className='w-screen h-20 flex justify-evenly items-center'>
-        
-        <div className="flex-1 pl-8">
-            <h2>NextDoor-Jobs</h2>
+    <div className='w-screen h-20 flex flex-1 items-center justify-between px-6'>
+
+    {/* left side of navbar */}
+    <div className='flex w-3/5'>
+
+        <div className="mr-14">
+            <h2 className='pl-6'>NextDoor-Jobs</h2>
         </div>
-        
-        <ul className='flex flex-1 '>
-            <li className='cursor-pointer'>Find Jobs</li>
-            <li className='pl-3 cursor-pointer'>Find Talents</li>
+
+
+        <ul className='flex items-center space-x-4'>
+            <li className='cursor-pointer'>Find Jobs <span className='text-gray-400'><FontAwesomeIcon icon={faAngleDown} className=' h-3'/></span></li>
+            <li className='cursor-pointer'>Find Talents <span className='text-gray-400'><FontAwesomeIcon icon={faAngleDown} className=' h-3'/></span></li>
+            <li className='cursor-pointer'>Manage Finances <span className='text-gray-400'><FontAwesomeIcon icon={faAngleDown} className=' h-3'/></span></li>
+            <li className='cursor-pointer'>Messages</li>
         </ul>
 
-       <div className='flex flex-1 items-center border-gray-400 border-2  w-8 rounded-3xl '>
-            <FontAwesomeIcon icon={faMagnifyingGlass} className='h-5 ml-1 text-gray-700' />
-            <form className='flex-grow ml-2 '>
-                <input type="text"  className='p-1 text-gray-600' placeholder='Search'/>
-            </form>
+    </div>
+        
+    {/* right side of navbar */}
+    <div className=' flex  items-center space-x-4 w-2/5' >
 
-            <h1 className='text-gray-200 pr-2 text-sm'>|</h1>
-
-            <div className='mr-4'>
-                <h4 className='p-1'>Jobs</h4>
+        <div className='w-3/5 flex items-center justify-between border-gray-400 border-2  rounded-3xl '>
+            <div className='flex items-center w-4/5'>
+                
+                    <FontAwesomeIcon icon={faMagnifyingGlass} className='h-4 ml-2 text-gray-600' />
+                    
+               
+                <form className=''>
+                    <input type="text"  className='p-1 text-gray-600 w-full' placeholder='Search'/>
+                </form>
             </div>
+
+            <div className='flex items-center w-1/5'>
+                <span className='text-gray-200 pl-0 pr-1 text-sm'>|</span>
+                <div className=''>
+                    <h4 className='p-1 cursor-pointer'>Jobs <span className='text-gray-400'><FontAwesomeIcon icon={faAngleDown} className=' h-3'/></span></h4>
+                </div>
+            </div>
+           
           
        </div>
+
+       <div className='relative h-5' >
+            <FontAwesomeIcon icon={faBell} className=' h-full cursor-pointer'/>
+            <span className='h-3 w-3 absolute -top-1 -right-1 bg-red-700 rounded-full '></span>
+       </div>
        
-       <FontAwesomeIcon icon={faBell} className='ml-4 h-5 cursor-pointer'/>
+       
 
      
 
-            <div className='flex flex-1'>
+            <div className='flex items-center'>
                 <img src={Profile} alt="profile image" className='w-10 h-10 rounded-full'/>
                 {/* <span>john doe</span> */}
             </div>
+    </div>
 
-           
-       
        
     
     </div>
@@ -51,3 +74,4 @@ function Navbar() {
 }
 
 export default Navbar
+
