d9060f2429a2ede93c8e94363d07c9162a5c7662
Kamau-ke <kamauwamkuu@gmail.com>
2024-12-13 09:22:23 +0300

    backend and frontend

--- backend/Controllers/job.js
+++ backend/Controllers/job.js
@@ -1,6 +1,12 @@
 const Job=require('../model/job')
 const {notFound, badRequest}=require('../errors/index')
 
+const createJob=async (req, res)=>{
+    req.body.createdBy=req.user.userID
+    const job=await Job.create({...req.body})
+    res.status(201).json({job})
+}
+
 const getAllJobs=async (req, res)=>{
     const jobs=await Job.find({})
     res.status(200).json({success:true, data:jobs}, {nHits:jobs.length})
@@ -22,5 +28,27 @@ const getJob=async (req,res)=>{
 }
 
 const editJob=async (req,res)=>{
-    
+    const {user:{userID},params:{id:jobId}, body:{title, description, budget, location,position, skills} }=req
+    const job=await Job.findOneAndUpdate({_id:jobId, createdBy:userID}, req.body, {new:true, runValidators:true})
+    res.status(200).json({job})
+}
+
+const deleteJob=async (req, res)=>{
+    const {user:{userID}, params:{id:jobId}}=req
+
+    const job=await Job.findOneAndDelete({_id:jobId, createdBy:userID})
+    if(!job){
+        throw new badRequest('Job not found')
+    }
+
+    res.status(200).json('Job deleted successfully')
+}
+
+module.exports={
+    createJob,
+    getAllJobs,
+    getUserJobs,
+    getJob,
+    editJob,
+    deleteJob
 }
