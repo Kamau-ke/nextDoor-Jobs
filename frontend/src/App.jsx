95b435551efd61f9f38e06726407ee3a11c480f3
Kamau-ke <kamauwamkuu@gmail.com>
2025-02-13 08:45:39 +0300

    job application page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,18 @@
-// import { useEffect, useState } from 'react'
-// import axios from 'axios'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-
-import Register from'./pages/register'
-import JobCard from './components/JobCard'
-
-import JobApplication from './pages/JobApplication'
-import Login from './pages/login'
-import PostJob from './pages/PostJob'
-import AboutJob from './pages/AboutJob'
-import HomePage from './pages/HomePage'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import JobApplication from './pages/JobApplication';
 
 function App() {
-  
-
   return (
     <div className='h-screen w-screen bg-white overflow-x-hidden'>
       <Router>
-      <Routes>
-      
-        <Route path='/' element={<HomePage/>} />
-        <Route path='/job/:id' element={JobApplication} />
-        
-      </Routes>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/job/:jobId" element={<JobApplication />} />
+        </Routes>
       </Router>
     </div>
   );
 }
 
-export default App
+export default App;
--- frontend/src/pages/JobApplication.jsx
+++ frontend/src/pages/JobApplication.jsx
@@ -1,25 +1,67 @@
 import React from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faDollarSign } from '@fortawesome/free-solid-svg-icons'
 import Button from '../components/button'
 function JobApplication() {
+    const {jobId}=useParams()
+    console.log(jobId);
+    
+
+    const [job, setJob]=useState({})
+    const [error, setError]=useState(null)
+    const [loading, setLoading]=useState(true)
+
+    const fetchData=async ()=>{
+        try {
+           const response=await axios.get(`http://localhost:5000/api/v1/job/${jobId}`) 
+           setJob(response.data.job[0])
+        
+           
+        } catch (error) { 
+            setError(error.message)
+        }
+    }
+
+    const dataLoaded=React.useRef(false)
+
+    useEffect(()=>{
+        if(!dataLoaded.current){
+            fetchData()
+            dataLoaded.current=true
+        }
+        
+    }, [])
+
+    console.log(job);
+    
+  
+    
+
+    
+    
+    // const job=axios.get('/api/v1/job/')
+
   return (
     <div className='overflow-x-hidden'>
         <div className='w-3/4 mx-auto mt-20 '>
             <h1 className='font-semibold text-3xl pb-10'>Submit a proposal</h1>
 
             {/* About job */}
+           
             <div className='border border-gray-300 rounded-3xl p-4'>   
                 <h2 className='font-medium text-2xl pb-8'>Job details</h2> 
                 <div className='flex'>
                     <div className='w-3/4 '>
-                    <h1 className='font-medium text-lg'>React Developer Needed</h1>
-                    <p>We need a React designer to quickly convert this nonfunctional design to working code for another developer to finish it. We want the chat functionality to work with local data, someone else will connect the API. However, we should see additional chats added like it would normally. We used a plugin to generate the code, so something may need to get cleaned up so it can be proper for another developer to finish working. We need someone who can work quick! We have a 4 hours turn around needed. This could turn into more work. To get an interview, explain what you have to do in your own words.</p>
+                    <h1 className='font-medium text-lg'>{job.title}</h1>
+                    <p>{job.description}</p>
                     </div>
 
                     <div className='border-l border-gray-300 ml-4 max-w-full'>
                     <FontAwesomeIcon icon={faDollarSign} className='ml-4 mt-4'/>
-                    <span>$10</span>
+                    <span>{job.budget}</span>
                     </div>
                 </div>
 
